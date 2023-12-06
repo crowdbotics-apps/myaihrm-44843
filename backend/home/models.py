65ed7f6794631b7182f5fcd90c693e7307299ddc
crowdbotics-apps <team@crowdbotics.com>
2023-12-06 19:39:02 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,25 @@
+from django.conf import settings
 from django.db import models
+
+
+class User(models.Model):
+    "Generated Model"
+    email = models.EmailField(
+        unique=True,
+        max_length=254,
+    )
+    first_name = models.CharField(
+        max_length=30,
+    )
+    last_name = models.CharField(
+        max_length=30,
+    )
+    is_active = models.BooleanField(
+        default="True",
+    )
+    is_staff = models.BooleanField(
+        default="False",
+    )
+    is_superuser = models.BooleanField(
+        default="False",
+    )
